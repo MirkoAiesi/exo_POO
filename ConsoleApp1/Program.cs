b321dae5e38bf786a541205d9a377c0d734d9a6d
Aiesi Mirko <m.aiesi@stag.technofuturtic.education>
2026-03-19 12:28:48 +0100

    Teste des deux méthodes dans le Program.CS

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,13 +2,20 @@
 
 Console.WriteLine("Hello, World!");
  
-Courant c = new Courant();
+Courant compte = new Courant();
+Epargne epargne = new Epargne();
 User personne = new User();
 
-c.Numero = "10.0";
+compte.Numero = "BE69 1430 7284 1452";
 personne.Name= "Aiesi";
 personne.Prenom = "Mirko";
+compte.Depot(50);
+epargne.Depot(500);
 
-c.Titulaire = personne ;
 
-Console.WriteLine(c.Titulaire.Name);
+compte.Titulaire = personne ;
+
+Console.WriteLine($"{personne.Name}, tu possèdes {compte.Solde} sur ton compte courrant {compte.Numero} et sur l'épargne {epargne.Solde}");
+compte.AppliquerInteret();
+epargne.AppliquerInteret();
+Console.WriteLine($"Solde sur le compte courrant apres les interet {compte.Solde} sur l'épargne {epargne.Solde}");
