c94381a1272ea1f939e6a89a8301dcf36ca27bae
Aiesi Mirko <m.aiesi@stag.technofuturtic.education>
2026-03-19 16:32:07 +0100

    Ajout ICustomer

---
+++ ConsoleApp1/Interfaces/ICustomer.cs
@@ -0,0 +1,8 @@
+﻿namespace ConsoleApp1.Interfaces;
+
+public interface ICustomer
+{
+    double Solde { get; }
+    bool Retrait(double montant);
+    bool Depot(double montant);
+}
--- ConsoleApp1/Models/Compte.cs
+++ ConsoleApp1/Models/Compte.cs
@@ -1,6 +1,8 @@
-﻿namespace ConsoleApp1.Models;
+﻿using ConsoleApp1.Interfaces;
 
-public abstract class Compte //abstract ne peut pas etre instancié, on ne peut pas créer d'objet compte
+namespace ConsoleApp1.Models;
+
+public abstract class Compte //abstract ne peut pas etre instancié, on ne peut pas créer d'objet compte utiliser a des fin d'héritage
 {
     public string? Numero { get; set; }
     public double Solde { get; protected set; }
